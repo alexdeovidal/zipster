2ab471eb5650c15872c1c86419935d8cbbcdceb3
Alex de 0. Vidal <contato@webav.com.br>
2025-03-30 22:50:33 -0300

    Add src/utils/templates/flutterTemplate.ts

---
+++ src/utils/templates/flutterTemplate.ts
@@ -0,0 +1,106 @@
+
+import { ProjectFile } from '../fileTypes';
+
+/**
+ * Generates a Flutter project template
+ * @returns Array of project files for a Flutter project
+ */
+export const generateFlutterProject = (): ProjectFile[] => {
+  return [
+    {
+      path: 'README.md',
+      content: `# Flutter Project
+
+This is a Flutter project template for cross-platform mobile development. Built with Dart and Material Design 3, supporting iOS, Android and web.
+
+## Getting Started
+
+1. Make sure Flutter is installed: https://flutter.dev/docs/get-started/install
+2. Run \`flutter pub get\` to install dependencies
+3. Run \`flutter run\` to launch the app on a connected device or simulator`
+    },
+    {
+      path: 'lib/main.dart',
+      content: `import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
+      home: const MyHomePage(title: 'Flutter Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}`
+    }
+  ];
+};
